c3ac69b48d32092a933fc96f09885b36eea94e5b
Carsefer <107973841+Carsefer@users.noreply.github.com>
2022-10-01 14:13:52 -0500

    change

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn, Diets } = require("./src/db.js");
-
+const { PORT } = process.env;
 const dietsDb = [
   "dairy free",
   "gluten free",
@@ -37,8 +37,8 @@ const dietsDb = [
 conn
   .sync({ force: false })
   .then(() => {
-    server.listen(3001, () => {
-      console.log("%s listening at 3001"); // eslint-disable-line no-console
+    server.listen(PORT, () => {
+      console.log(`% listening at ${PORT}`); // eslint-disable-line no-console
     });
   })
   .then(() => {
